9e519915929b11173512d221a3caa054288e2a95
Naoki0722 <mattu.nao722@gmail.com>
2022-03-04 08:13:48 +0900

    initialize project app

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,18 @@
 import express from 'express';
-import mysql from 'mysql2';
 
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000
-// const connection = mysql.createConnection(process.env.DATABASE_URL)
-// connection.connect()
-// app.use(express.json());
 
+
+app.get('/',(req: express.Request, res: express.Response): void => {
+    res.send('Hello World')
+});
 
 app.get('/api/sample',(req: express.Request, res: express.Response): void => {
-    // パラメーターを取得
-    console.log(req.query);
     res.json({
       message: 'Hello, world! GET',
     });
-    // connection.query('SELECT * FROM users', function (err, rows, fields) {
-    //   if (err) throw err
-  
-    //   res.send(rows)
-    // })
 });
 
 app.all('*', (_req: express.Request, res: express.Response): void => {
